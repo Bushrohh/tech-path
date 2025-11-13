1c535f93bfbef8e1fd6be34b1cf38e4bbedca134
Bushrohh <sekerebushroh@gmail.com>
2025-11-12 23:36:38 -0800

    created notfound

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main className="pt-20 p-6 space-y-2">
+      <h1 className="text-2xl font-semibold">Page not found</h1>
+      <p>Sorry, we couldn’t find that page.</p>
+      <a href="/" className="text-blue-600 underline">Go home</a>
+    </main>
+  );
+}
